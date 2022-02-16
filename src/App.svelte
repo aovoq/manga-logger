fec4fd17e7e05343a0ae087bf4d4241a1883b24e
aovoq <a21dc163@dhw.ac.jp>
2022-02-16 23:51:56 +0900

    ADD: PAGE LINK

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,8 @@
    import Register from './Components/Auth/Register.svelte'
    import Login from './Components/Auth/Login.svelte'
    import Dashboard from './Components/Dashboard.svelte'
+   import Add from './Components/Comic/Add.svelte'
+   import List from './Components/Comic/List.svelte'
 
    user.set(supabase.auth.user())
 
@@ -31,15 +33,25 @@
          <li><Link to="login">Login</Link></li>
          <li><Link to="register">Register</Link></li>
          <li><a href="/" on:click|preventDefault={signOut}>Logout</a></li>
+         <li><Link to="add">Add</Link></li>
+         <li><Link to="list">List</Link></li>
       </ul>
    </nav>
    <main>
       <Route path="login" component={Login} />
       <Route path="register" component={Register} />
       <Route path="dashboard" component={Dashboard} />
+      <Route path="add" component="{Add}" />
+      <Route path="list" component="{List}" />
       <Route path="/">
          <h1>Home</h1>
          <p>To view the protected content, register or login to your account</p>
       </Route>
    </main>
 </Router>
+
+<style>
+   li {
+       display: inline;
+   }
+</style>
