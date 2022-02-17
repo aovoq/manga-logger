e3c7f5231158b8ecf2fa8e487113f680af229a59
aovoq <a21dc163@dhw.ac.jp>
2022-02-17 23:21:33 +0900

    FIX: ROUTING SYSTEM

--- src/App.svelte
+++ src/App.svelte
@@ -1,57 +1,79 @@
 <script>
-   import { Router, Link, Route } from 'svelte-routing'
-   import { user } from './sessionStore'
-   import { supabase } from './supabaseClient'
+   import { Router, Link, Route, navigate } from 'svelte-routing'
+   import { user } from './lib/sessionStore.js'
+   import { supabase } from './lib/supabaseClient.js'
    import Register from './Components/Auth/Register.svelte'
    import Login from './Components/Auth/Login.svelte'
    import Dashboard from './Components/Dashboard.svelte'
-   import Add from './Components/Comic/Add.svelte'
-   import List from './Components/Comic/List.svelte'
+   import Profile from './Components/Auth/Profile.svelte'
+   import FirstView from './Components/Auth/FirstView.svelte'
+   import Redirect from './Components/Redirect.svelte'
 
    user.set(supabase.auth.user())
 
-   supabase.auth.onAuthStateChange((_, session) => {
-      user.set(session.user)
+   supabase.auth.onAuthStateChange((event, session) => {
+      if (event === 'SIGNED_IN') user.set(session.user)
    })
 
-   const signOut = async () => {
-      try {
-         let { error } = await supabase.auth.signOut()
-         user.set(null)
-         if (error) throw error
-      } catch (error) {
-         console.error(error)
-      }
+   const signOut = () => {
+      supabase.auth.signOut()
+      user.set(false)
+      navigate('/')
    }
 </script>
 
+<svelte:head>
+   <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
+</svelte:head>
+
 <Router>
-   <nav>
-      <ul>
-         <li><Link to="/">Home</Link></li>
-         <li><Link to="dashboard">Dashboard</Link></li>
-         <li><Link to="login">Login</Link></li>
-         <li><Link to="register">Register</Link></li>
-         <li><a href="/" on:click|preventDefault={signOut}>Logout</a></li>
-         <li><Link to="add">Add</Link></li>
-         <li><Link to="list">List</Link></li>
-      </ul>
-   </nav>
+   {#if $user}
+      <nav>
+         <ul>
+            <li><a href="/" on:click|preventDefault={signOut}>Logout</a></li>
+            <li>
+               <Link to="dashboard">Dashboard</Link>
+            </li>
+            <li>
+               <Link to="profile">Profile</Link>
+            </li>
+         </ul>
+      </nav>
+   {/if}
+
    <main>
-      <Route path="login" component={Login} />
-      <Route path="register" component={Register} />
-      <Route path="dashboard" component={Dashboard} />
-      <Route path="add" component="{Add}" />
-      <Route path="list" component="{List}" />
-      <Route path="/">
-         <h1>Home</h1>
-         <p>To view the protected content, register or login to your account</p>
-      </Route>
+      {#if $user}
+         <Route path="dashboard" component={Dashboard}/>
+         <Route path="profile" component="{Profile}"/>
+      {:else}
+         <Route path="login" component={Login}/>
+         <Route path="register" component={Register}/>
+         <Route path="/" component="{FirstView}"/>
+      {/if}
+      <Route path="*" component="{Redirect}"/>
    </main>
 </Router>
 
 <style>
-   li {
-       display: inline;
-   }
+    ul {
+        display: flex;
+        justify-content: space-between;
+        padding: 0 30px;
+    }
+
+    :global(html) {
+        font-size: 62.5%;
+    }
+
+    :global(body) {
+        background: #F6F6F6;
+        font-family: Roboto, sans-serif;
+        font-weight: bold;
+    }
+
+    :global(*, *::before, *::after) {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
 </style>
