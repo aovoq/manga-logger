6daa58c47e1c1ad30e26f26f444c1900e0153ba8
aovoq <a21dc163@dhw.ac.jp>
2022-02-17 23:17:20 +0900

    FIX: Add_FUNCTION INPUT TYPE

--- src/Components/Comic/Add.svelte
+++ src/Components/Comic/Add.svelte
@@ -1,17 +1,18 @@
 <script>
-   import { supabase } from '../../supabaseClient'
-   import { user } from '../../sessionStore'
+   import { supabase } from '../../lib/supabaseClient.js'
+   import { user } from '../../lib/sessionStore.js'
 
    let title, desc, number
    let loading = false
    const handleSubmit = async() => {
       try {
          loading = true
+         if (title === undefined || desc === undefined || number === undefined) throw 'Please input field'
          const {error } = await supabase.from('comic_log')
             .insert([{title, desc, number, user_id: $user.id}])
          if (error) throw error
       } catch (e) {
-         console.error(e.message)
+         console.error(e)
       } finally {
          loading = false
       }
@@ -21,8 +22,18 @@
 
 <h1>Add</h1>
 <form on:submit|preventDefault={handleSubmit}>
-   <input type="text" name="title" bind:value={title}>
-   <input type="text" name="desc" bind:value={desc}>
-   <input type="number" name="text" bind:value={number}>
+   <input type="text" name="title" bind:value={title} placeholder="title">
+   <input type="text" name="desc" bind:value={desc} placeholder="desc">
+   <input type="text" name="number" bind:value={number} placeholder="vol.">
    <input type="submit" value="add" disabled={loading}>
 </form>
+
+<style>
+    form {
+       display: flex;
+       justify-content: space-between;
+   }
+    input {
+        width: 25%;
+    }
+</style>
