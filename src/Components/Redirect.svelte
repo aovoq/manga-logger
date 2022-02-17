a45158a1781513437cf0e7f3ea2fa324a0c108a5
aovoq <a21dc163@dhw.ac.jp>
2022-02-17 23:20:34 +0900

    ADD: ROUTER REDIRECT FUNCTION

---
+++ src/Components/Redirect.svelte
@@ -0,0 +1,15 @@
+<script>
+   import { onMount } from 'svelte'
+   import { navigate } from 'svelte-routing'
+   import {user} from '../lib/sessionStore.js'
+
+   onMount(() => {
+      if ($user) {
+         navigate('/dashboard')
+      } else {
+         navigate('/')
+      }
+   })
+</script>
+
+<div>404</div>
